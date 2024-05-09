b53a38affe0c724f8fb0c53a77bed476db7a7604
prikol769 <prikol769@gmail.com>
2024-05-09 12:59:12 -0500

    added AnimatedCounter component

---
+++ components/AnimatedCounter.tsx
@@ -0,0 +1,11 @@
+import CountUp from "react-countup";
+
+const AnimatedCounter = ({ amount }: { amount: number }) => {
+  return (
+    <div className="w-full">
+      <CountUp end={amount} decimal="," decimals={2} prefix="$" />
+    </div>
+  );
+};
+
+export default AnimatedCounter;
